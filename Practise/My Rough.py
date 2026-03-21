05f2f7685de67336b11baafd89b0501fab1e614f
Sudha1409 <sudhaparimi@yahoo.com>
2026-03-21 18:50:56 +0000

    Day 7

---
+++ Practise/My Rough.py
@@ -0,0 +1,27 @@
+
+print("My practise session")
+
+a = 4
+b = 5
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a // b
+h = a % b
+
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+
+
+print("***********      Input    *************")
+print(" ")
+print(" ")
+print(" ")
+age = input("Enter your age : ")
+print("age is", age)
